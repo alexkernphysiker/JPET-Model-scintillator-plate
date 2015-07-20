acc3ad13f9bf1fb48959ba051416f369943527c7
alexkernphysiker <kernphysiker@openmailbox.org>
2015-07-20 22:45:24 +0200

    throw fix

--- model1.cpp
+++ model1.cpp
@@ -11,25 +11,33 @@ int main(int,char**){
 	double width=20;
 	double height=5;
 	double step=20;
-	
+	printf("CREATE\n");
 	BC420 scintillator({make_pair(0,length),make_pair(0,width),make_pair(0,height)});
-	
 	typedef shared_ptr<SiliconPhm> PHM;vector<PHM> matrix;
 	for(double x=step/2.0;x<length;x+=step)matrix.push_back(TestPhm({x,width/2.0},step));
 	for(PHM phm:matrix)scintillator.Surface(2,RectDimensions::Left)<<phm;
-	
-	for(unsigned int cnt=0;cnt<100;cnt++)scintillator.RegisterGamma({length/2.0,width/2.0,height/2.0},3000);
-	
+	printf("BEGIN\n");
+	for(unsigned int cnt=0;cnt<250;cnt++){
+		scintillator.RegisterGamma({length/2.0,width/2.0,height/2.0},3000);
+		printf("N=%i\r",cnt+1);
+	}
+	printf("FILL\n");
 	typedef LinearInterpolation<double> Tbl;
 	typedef PlotPoints<double,Tbl> PlotTbl;
 	Tbl count,counterror,time,timeerror;
 	for(PHM phm:matrix){
 		double x=phm->pos()[0];
-		count<<make_pair(x,phm->counter().average());
-		counterror<<make_pair(x,phm->counter().sigma());
-		time<<make_pair(x,phm->timer().average());
-		timeerror<<make_pair(x,phm->timer().sigma());
+		if(phm->counter().events_count()>=2){
+			count<<make_pair(x,phm->counter().average());
+			counterror<<make_pair(x,phm->counter().sigma());
+		}
+		if(phm->timer().events_count()>=2){
+			time<<make_pair(x,phm->timer().average());
+			timeerror<<make_pair(x,phm->timer().sigma());
+		}
 	}
+	printf("PLOT\n");
 	PlotTbl().WithErrorOnX("Photoelectrons",static_left(count),counterror.func());
 	PlotTbl().WithErrorOnX("Time",static_left(time),timeerror.func());
+	printf("END\n");
 }
