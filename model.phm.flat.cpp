9c415df29fd55f91c7fb48d160c32be262ce3253
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-20 16:25:23 +0200

    model optimization

--- model.phm.flat.cpp
+++ model.phm.flat.cpp
@@ -13,26 +13,18 @@ int main(int,char**){
 	BC420 scintillator({make_pair(0,ScinSize[0]),make_pair(0,ScinSize[1]),make_pair(0,ScinSize[2])});
 	auto output=make_shared<SignalsToFile>();
 	{
-		auto first=make_shared<SignalSortAndSelect2>(0),
-			second=make_shared<SignalSortAndSelect2>(1),
-			third=make_shared<SignalSortAndSelect2>(2);
+		auto sort=make_shared<SignalSort2>();
 		for(double x=PhmStep_flat[0]/2.0;x<ScinSize[0];x+=PhmStep_flat[0])
 				for(double y=PhmStep_flat[1]/2.0;y<ScinSize[1];y+=PhmStep_flat[1]){
 					auto phm=hamamatsu({x,y},1.0);
 					scintillator.Surface(2,RectDimensions::Left)>>phm;
-					first<<phm->Time();
-					second<<phm->Time();
-					third<<phm->Time();
+					sort<<phm->Time();
 				}
-		auto index1=make_shared<Signal>(),time1=make_shared<Signal>(),
-			index2=make_shared<Signal>(),time2=make_shared<Signal>(),
-			index3=make_shared<Signal>(),time3=make_shared<Signal>();
-		first>>index1>>time1;
-		output<<index1<<time1;
-		second>>index2>>time2;
-		output<<index2<<time2;
-		third>>index3>>time3;
-		output<<index3<<time3;
+		for(size_t i=0;i<8;i++){
+			auto signal=make_shared<Signal>();
+			sort>>signal;
+			output<<signal;
+		}
 	}
 	uniform_real_distribution<double> distrz(0,ScinSize[2]);
 	for(double x=PosStep[0];x<ScinSize[0];x+=PosStep[0])
