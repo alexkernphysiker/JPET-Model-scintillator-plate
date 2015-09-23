25633106b94faa05d5f9fcb5a1975eeb085c307f
alexkernphysiker <kernphysiker@openmailbox.org>
2015-09-23 12:15:02 +0200

    model with photomultipliers on the flat side has been updated

--- model.phm.flat.cpp
+++ model.phm.flat.cpp
@@ -12,9 +12,7 @@ int main(int,char**){
 	RANDOM engine;
 	BC420 scintillator({make_pair(0,ScinSize[0]),make_pair(0,ScinSize[1]),make_pair(0,ScinSize[2])});
 	auto output=make_shared<SignalsToFile>();
-	auto output_triggerless=make_shared<SignalsToFile>();
 	{
-		auto trigger=make_shared<AllSignalsPresent>();
 		for(size_t dimension=0;dimension<2;dimension++){
 			for(auto side=RectDimensions::Left;side<=RectDimensions::Right;inc(side)){
 				auto allside=make_shared<SignalSortAndSelect2>(0);size_t cnt=0;
@@ -28,9 +26,7 @@ int main(int,char**){
 				auto index=make_shared<Signal>(),time=make_shared<Signal>(),
 				index_triggered=make_shared<Signal>(),time_triggered=make_shared<Signal>();
 				allside>>index>>time;
-				(trigger<<index<<time)>>index_triggered>>time_triggered;
-				output<<index_triggered<<time_triggered;
-				output_triggerless<<index<<time;
+				output<<index<<time;
 			}
 		}{
 			auto allside=make_shared<SignalSortAndSelect2>(0);size_t cnt=0;
@@ -44,23 +40,19 @@ int main(int,char**){
 			auto index=make_shared<Signal>(),time=make_shared<Signal>(),
 			index_triggered=make_shared<Signal>(),time_triggered=make_shared<Signal>();
 			allside>>index>>time;
-			(trigger<<index<<time)>>index_triggered>>time_triggered;
-			output<<index_triggered<<time_triggered;
-			output_triggerless<<index<<time;
+			output<<index<<time;
 		}
 	}
 	uniform_real_distribution<double> distrz(0,ScinSize[2]);
 	for(double x=PosStep[0];x<ScinSize[0];x+=PosStep[0])
 		for(double y=PosStep[1];y<ScinSize[1];y+=PosStep[1]){
 			ostringstream name,name2;
-			name<<"DATA."<<x<<"."<<y<<".edges_plus_flat.txt";
-			name2<<"DATA."<<x<<"."<<y<<".edges_plus_flat.triggerless.txt";
+			name<<"DATA."<<x<<"."<<y<<".flat.txt";
 			output->Redirect(name.str());
-			output_triggerless->Redirect(name2.str());
 			printf("BEGIN %s\n",name.str().c_str());
 			for(size_t cnt=0;cnt<ev_n;cnt++)
 				scintillator.RegisterGamma({x,y,distrz(engine)},3000,engine);
 			printf("END %s\n",name.str().c_str());
 		}
-		printf("GOODBYE!\n");
+	printf("GOODBYE!\n");
 }
